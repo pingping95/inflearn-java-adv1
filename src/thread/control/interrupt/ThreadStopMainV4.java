292bd50b9728072464b3e9583d6a74f309830627
pingping95 <xogns556@naver.com>
2025-03-10 08:06:49 +0900

    first commit

---
+++ src/thread/control/interrupt/ThreadStopMainV4.java
@@ -0,0 +1,45 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV4 {
+
+    public static void main(String[] args) {
+
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(100);
+        log("작업 중단 지시 - thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            // Thread.interrupted() : 인터럽트 상태 반환하면서 상태를 그 반대로 변경
+            while (!Thread.interrupted()) {
+                log("작업 중");
+            }
+
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+
+            try {
+                log("자원 정리 시도");
+                Thread.sleep(1000);
+                log("자원 정리 완료");
+
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
+
+            }
+            log("작업 종료");
+        }
+    }
+}
